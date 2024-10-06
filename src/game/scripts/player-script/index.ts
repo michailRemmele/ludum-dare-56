1d236114c2f56cb9f458f312f4930390754185e9
Mikhail Remmele <mikhail.remmele@gmail.com>
2024-10-06 09:56:39 +0300

    adjust camera zoom

--- src/game/scripts/player-script/index.ts
+++ src/game/scripts/player-script/index.ts
@@ -5,12 +5,14 @@ import type {
 } from 'remiz';
 import {
   Script,
+  Camera,
 } from 'remiz';
 
 import { Money } from '../../components';
 import * as EventType from '../../events';
 
 const MONEY_PER_ENEMY = 10;
+const VIEWPORT_SIZE = 480;
 
 export class PlayerScript extends Script {
   private actor: Actor;
@@ -34,6 +36,15 @@ export class PlayerScript extends Script {
     money.value += MONEY_PER_ENEMY;
     this.scene.dispatchEvent(EventType.MoneyUpdate, { money: money.value });
   };
+
+  private updateZoom(): void {
+    const camera = this.actor.getComponent(Camera);
+    camera.zoom = Math.min(camera.windowSizeX / VIEWPORT_SIZE, camera.windowSizeY / VIEWPORT_SIZE);
+  }
+
+  update(): void {
+    this.updateZoom();
+  }
 }
 
 PlayerScript.scriptName = 'PlayerScript';
